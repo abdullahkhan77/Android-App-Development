f94521b526a426a2cd31352b46cda8cc6059624a
abdullahcui777@gmail.com <abdullahcui777@gmail.com>
2021-10-17 21:37:04 +0500

    Calculator

---
+++ Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun numberEvent(view: View) {
+
+        var buselect=view as Button
+    }
+}
